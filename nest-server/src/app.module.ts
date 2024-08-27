b4c765112ce3734cde40296a4d76b3a91e49e2cc
Tom <godppun@naver.com>
2024-08-27 13:53:34 +0900

    Middleware 기능 추가

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,13 +1,33 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatController } from './cat/cat.controller.spec';
-import { CatsService } from './cat/cat.service';
 import { CatModule } from './cat/cat.module';
+import { CatsService } from './cat/cat.service';
+import { LoggerMiddleware } from './logger.middleware.ts';
 
 @Module({
   imports: [CatModule],
   controllers: [AppController, CatController],
   providers: [AppService, CatsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(LoggerMiddleware).forRoutes(CatController);
+    consumer
+      // .apply(logger) // Functional middleware
+      // .apply(cores(), helmet(), logger) // Multiple middleware
+      .apply(LoggerMiddleware)
+      .exclude(
+        { path: 'cat', method: RequestMethod.PUT },
+        { path: 'cat', method: RequestMethod.POST },
+        'cat/(.*)',
+      )
+      .forRoutes({ path: 'cat', method: RequestMethod.GET });
+  }
+}
--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  /**
+   * Global Middleware
+   * Accessing the DI container in a global middleware is not possible.
+   * You can use a functional middleware instead when using app.use().
+   * Alternatively, you can use a class middleware and consume it with .
+   * forRoutes('*') within the AppModule (or any other module).
+   */
+  // app.use(logger); // Global Middleware
   await app.listen(4000);
 }
 bootstrap();
